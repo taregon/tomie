e2d75e6dfb0b686bb39621decdd8f5b8a6bcefa6
Irving O <113083219+taregon@users.noreply.github.com>
2024-04-15 22:20:12 -0500

    Update aspirina.bat
    Se agregan comentarios

--- aspirina.bat
+++ aspirina.bat
@@ -1,4 +1,13 @@
+@echo off
+
+REM 1. Ejecuta el Comprobador de archivos de sistema para verificar y reparar archivos del sistema.
 sfc /scannow
+
+REM 2. Verifica la integridad de la imagen de Windows. Solo informa sobre el estado.
 DISM /Online /Cleanup-Image /CheckHealth
+
+REM 3. Realiza un escaneo más detallado de la imagen de Windows para buscar corrupción.
 DISM /Online /Cleanup-Image /ScanHealth
+
+REM 4. Intenta reparar automáticamente cualquier corrupción encontrada en la imagen de Windows.
 DISM /Online /Cleanup-Image /RestoreHealth
